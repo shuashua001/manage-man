799820e7801bda3793462158b674a7a9ab3825ed
shuashua <936606041@qq.com>
2023-09-13 11:23:00 +0800

    完成home的柱状图以及饼状图

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -9,8 +9,12 @@
         <Order />
         <el-card style="height: 280px" id="linear"></el-card>
         <div class="graph">
-          <el-card style="height: 260px"></el-card>
-          <el-card style="height: 260px"></el-card>
+          <el-card style="height: 260px">
+            <div ref="columnar" style="height: 260px"></div>
+          </el-card>
+          <el-card style="height: 260px">
+            <div ref="pancake" style="height: 240px"></div>
+          </el-card>
         </div>
       </el-col>
     </el-row>
@@ -22,7 +26,6 @@ import Userinfo from "./components/userinfo.vue";
 import Purchased from "./components/purchased.vue";
 import Order from "./components/order.vue";
 import * as echarts from "echarts";
-import { mapState } from "vuex";
 
 export default {
   components: {
@@ -32,13 +35,15 @@ export default {
   },
   computed: {
     getOrderData() {
-      return this.$store.state.home.homeData.orderData;
+      return this.$store.state.home.homeData;
     }
   },
   watch: {
     getOrderData(newval, oldval) {
-      // console.log(newval, oldval);
-      this.writeLinear(newval);
+      // console.log(newval);
+      this.writeLinear(newval.orderData);
+      this.writeColumnar(newval.userData);
+      this.writePancake(newval.videoData);
     }
   },
   methods: {
@@ -64,6 +69,79 @@ export default {
         });
       });
       linear.setOption(linearOption);
+    },
+    writeColumnar(userData) {
+      const columnar = echarts.init(this.$refs.columnar);
+      const columnarOption = {
+        legend: {
+          textStyle: {
+            color: "#333"
+          }
+        },
+        gird: {
+          left: "20%"
+        },
+        tooltip: {
+          trigger: "axis"
+        },
+        xAxis: {
+          type: "category",
+          data: userData.map(item => item.data),
+          axisLine: {
+            lineStyle: {
+              color: "#17b3a3"
+            }
+          },
+          axisLabel: {
+            interval: 0,
+            color: "#333"
+          }
+        },
+        yAxis: {
+          type: "value",
+          axisLine: {
+            lineStyle: {
+              color: "#17b3a3"
+            }
+          }
+        },
+        color: ["#2ec7c9", "#b6a2de"],
+        series: [
+          {
+            name: "新增用户",
+            type: "bar",
+            data: userData.map(item => item.new)
+          },
+          {
+            name: "活跃用户",
+            type: "bar",
+            data: userData.map(item => item.active)
+          }
+        ]
+      };
+      columnar.setOption(columnarOption);
+    },
+    writePancake(videoData) {
+      const pancake = echarts.init(this.$refs.pancake);
+      const pancakeOption = {
+        tooltip: {
+          trigger: "item"
+        },
+        color: [
+          "#0f78f4",
+          "#dd536b",
+          "#9462e5",
+          "#a6a6a6",
+          "#e1bb22",
+          "#39c362",
+          "#3ed1cf"
+        ],
+        series: {
+          type: "pie",
+          data: videoData
+        }
+      };
+      pancake.setOption(pancakeOption);
     }
   },
   mounted() {
