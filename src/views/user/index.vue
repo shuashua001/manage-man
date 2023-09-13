db480ac60afe113aead3abae6aee7d0658a130f3
shuashua <936606041@qq.com>
2023-09-13 16:48:50 +0800

    完成user界面新增表单验证功能

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,32 +1,32 @@
 <template>
   <div>
     <el-button type="primary" @click="dialogVisible = true">+ 新增</el-button>
-    <el-dialog title="提示" :visible.sync="dialogVisible" width="50%">
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="50%" :before-close="handleClose">
       <!-- 新增表单 -->
-      <el-form ref="form" :model="form" label-width="100px" :inline="true">
-        <el-form-item label="姓名">
+      <el-form ref="form" :model="form" :rules="rules" label-width="100px" :inline="true">
+        <el-form-item label="姓名" prop="name">
           <el-input v-model="form.name" placeholder="请输入姓名"></el-input>
         </el-form-item>
-        <el-form-item label="年龄">
+        <el-form-item label="年龄" prop="age">
           <el-input v-model="form.age" placeholder="请输入年龄"></el-input>
         </el-form-item>
-        <el-form-item label="性别">
+        <el-form-item label="性别" prop="sex">
           <el-select v-model="form.sex" placeholder="请选择">
             <el-option label="男" value="1"></el-option>
             <el-option label="女" value="0"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="出生日期">
+        <el-form-item label="出生日期" prop="birth">
           <el-date-picker v-model="form.birth" type="date" placeholder="选择日期"></el-date-picker>
         </el-form-item>
-        <el-form-item label="地址">
+        <el-form-item label="地址" prop="addr">
           <el-input v-model="form.addr" placeholder="请输入地址"></el-input>
         </el-form-item>
       </el-form>
 
       <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        <el-button @click="cancel()">取 消</el-button>
+        <el-button type="primary" @click="submit()">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -43,8 +43,34 @@ export default {
         sex: "",
         birth: "",
         addr: ""
+      },
+      rules: {
+        name: [{ required: true, message: "请输入姓名" }],
+        age: [{ required: true, message: "请输入年龄" }],
+        sex: [{ required: true, message: "请选择性别" }],
+        birth: [{ required: true, message: "请选择出生如期" }],
+        addr: [{ required: true, message: "请填写地址" }]
       }
     };
+  },
+  methods: {
+    submit() {
+      // console.log(this.$refs.form);
+      this.$refs.form.validate(success => {
+        // console.log(success);
+        if (success) {
+          this.dialogVisible = false;
+          this.$refs.form.resetFields();
+        }
+      });
+    },
+    cancel() {
+      this.dialogVisible = false;
+      this.$refs.form.resetFields();
+    },
+    handleClose() {
+      this.cancel();
+    }
   }
 };
 </script>
