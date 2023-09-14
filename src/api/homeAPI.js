6bb15ce12a594297269a58e81180a5c8a4af202d
shuashua <936606041@qq.com>
2023-09-14 10:32:44 +0800

    完成user页面后端数据以及前端接口编写

--- src/api/homeAPI.js
+++ src/api/homeAPI.js
@@ -6,3 +6,35 @@ export const getHomeDataApi = ()=> {
     url: '/home'
   })
 }
+
+export const getUserDataApi = (params)=> {
+  return http({
+    method: 'get',
+    url: '/user/getUser',
+    params
+  })
+}
+
+export const addUserApi = (data)=> {
+  return http({
+    method: 'post',
+    url: '/user/add',
+    data
+  })
+}
+
+export const delUserDataApi = (data)=> {
+  return http({
+    method: 'post',
+    url: '/user/del',
+    data
+  })
+}
+
+export const editUserDataApi = (data)=> {
+  return http({
+    method: 'post',
+    url: '/user/edit',
+    data
+  })
+}
--- src/api/mock.js
+++ src/api/mock.js
@@ -1,4 +1,10 @@
 import Mock from "mockjs";
 import homeData from "./mockData/homeData";
+import userData from "./mockData/userData";
 
 Mock.mock('/local/home','get',homeData.getStatiData)
+
+Mock.mock('/local/user/getUser','get',userData.getUserList)
+Mock.mock('/local/user/add','post',userData.createUser)
+Mock.mock('/local/user/del','post',userData.deleteUser)
+Mock.mock('/local/user/edit','post',userData.updateUser)
